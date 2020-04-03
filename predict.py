64f97654cb1122b4404afc95b57a15ce2af99b34
Ophelia <co0758442@gmail.com>
2020-04-03 21:15:59 +0100

    Cleaned code, added comments

--- predict.py
+++ predict.py
@@ -9,61 +9,87 @@ import pyodbc
 from datetime import datetime
 import sys
 
-
+# Warnings arise when models are run on different versions of python 3 but 
+# cause no operational effects.
 warnings.filterwarnings("ignore")
-reddit = praw.Reddit(client_id=Secrets.cid, \
-                     client_secret=Secrets.clientSec, \
-                     user_agent='streamba_demo', \
-                     username='streamba_demo', \
-                     password=Secrets.redditPass)
+
+# Connecting to Reddit client.
+reddit = praw.Reddit(client_id = Secrets.cid, \
+                     client_secret = Secrets.clientSec, \
+                     user_agent = 'streamba_demo', \
+                     username = 'streamba_demo', \
+                     password = Secrets.redditPass)
 
 
+# Loading Models
 cv = pickle.load(open(".\\Modelling\\cv.pkl", 'rb'))
-model=pickle.load(open(".\\Modelling\\model.pkl",'rb'))
+model = pickle.load(open(".\\Modelling\\model.pkl",'rb'))
+
+# Connecting to AWS MSSQL server
 con = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};'
-                     'SERVER='+Secrets.server+';UID='
-                     +Secrets.username+
-                     ';PWD='+
-                     Secrets.redditPass+';Initial Catalog=StreamDemo')
+                     'SERVER=' + Secrets.server + ';UID='
+                     + Secrets.username + ';PWD=' +
+                     Secrets.redditPass + ';Initial Catalog=StreamDemo')
 cur = con.cursor()
 
+# Description: Inserts new values into the SQL database
+# 
+# Variables
+#   link (string) - link used in Reddit lookup.
+#   pos (int) - The number of positive classified comments.
+#   neg (int) - The number of negatively classified comments.
+#   results (string) - The over all majority result for the classification i.e.
+#                      if there are more positve than negative we  say positive
+#                      or none if its a tie.
 
 def insertTab(link,pos,neg,result):
     cur.execute('INSERT INTO StreamDemo.dbo.redditTable'
                 '(link,positive_comments,negative_comments,overall_majority)'
-                'VALUES (\''+link+"\',"+str(pos)+","+str(neg)+",\'"+result+"\');")
+                'VALUES (\''+link+"\'," + str(pos) + ","+str(neg) + ",\'" + result + "\');")
     con.commit()
     
+
+# Description: Using our model, classifys values into positive or negative
+#              also prints result to screen.
+# 
+# Variables
+#   link (string) - Link to Reddit post.
 
 def postAnalysis(link):
     print("Performing post analysis for")
     print(link)
     print("\n")
     print(datetime.now())
-    l=[]
+    l = []
     submission = reddit.submission(url=link)
     for comment in submission.comments.list():
         if isinstance(comment, MoreComments):
             continue
+        
+        # For large comment threads the extra comments dont provide much 
+        # additional information and can slow down prediction dramaticaly
         elif len(l)>1000:
             continue
         l.append(utils.cleanText(comment.body))
     
+    # If the thread contains no comments 
     if (len(l))<1:
         insertTab(link,0,0,"None")
         print("No comments found")
         return
     
     preds=model.predict(cv.transform(l))
-    pos=(preds==1).sum()
-    neg=(preds==0).sum()
+    pos = (preds==1).sum()
+    neg = (preds==0).sum()
+    
     if pos>neg:
         insertTab(link,0,0,"Positive")
         print("Result:Majority Positive \n")
+    
     elif pos<neg:
         insertTab(link,0,0,"Negative")
-        
         print("Result:Majority Negative \n")
+    
     else:
         insertTab(link,pos,neg,"None")
  
--- training.py
+++ training.py
@@ -1,6 +1,6 @@
 from pandas import read_csv
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.naive_bayes import MultinomialNB
+from sklearn.linear_model import LogisticRegression
 
 from numpy import array
 import pickle
@@ -8,29 +8,51 @@ import warnings
 warnings.filterwarnings("ignore")
 import utils
 
+
 stopwords = set(utils.stopwords.words('english') + list(utils.punctuation) + ['AT_USER','URL'])
-dataset_path=".\\TrainingData\\training.1600000.processed.noemoticon.csv"
+
+# Training data not included in git as its too large
+dataset_path = ".\\TrainingData\\training.1600000.processed.noemoticon.csv"
+
+
+# Description: Loads training data into df, removing irrelevant coloumns and 
+#              converting the target variables into something more workable.
+# 
+# Variables
+#   filepath (string) - Filepath to training data
 
 def saveDataset(filepath):
     colNames = ["target", "ids", "date", "flag", "user", "text"]
     df = read_csv(dataset_path, encoding="ISO-8859-1", names=colNames)
-    df=df.drop(["ids","flag","date","user"], axis = 1)
-    df.target=df.target.replace(4,1)
+    df = df.drop(["ids","flag","date","user"], axis = 1)
+    df.target = df.target.replace(4,1)
     return df
+
+# Description: Takes in our training data and models using a Naive Bayes model.
+#              saves as pickle files to be used by our predictors.
+
 
 def trainModel():
     print("Running trainModel:")
     print("Loading data")
-    df= saveDataset(dataset_path)                 
-    df["text"]=df["text"].apply(utils.cleanText)
-    tweets=df['text'].tolist()
+    df = saveDataset(dataset_path)                 
+    df["text"] = df["text"].apply(utils.cleanText)
+    tweets = df['text'].tolist()
     
     print("Vectorising")
-    cv=CountVectorizer(max_df=0.85,stop_words=stopwords,max_features=10000)
-    word_count_vector=cv.fit_transform(tweets)
+    
+    # Data must be converted to matrix format to be readable by our model. 
+    # Keeping our features low prevents the model from becoming overcomplicated
+    cv = CountVectorizer(max_df=0.85,stop_words=stopwords,max_features=10000)
+    word_count_vector = cv.fit_transform(tweets) 
     
     print("Fitting model")
-    model = MultinomialNB()
+    model = LogisticRegression(C=1.) # Logistic regression provided a slightly 
+    #                                  better auc score compared to NB 
+    #                                 (0.878>0.877). I tried to keep models simple
+    
+    # model = MultinomialNB()
+    
     model.fit(word_count_vector,array(df["target"]))
     
     print("Saving model")
